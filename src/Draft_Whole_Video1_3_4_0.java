1e9fe0e7db59df7416d7792419bd9cd7df025233
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-15 22:57:06 +0300

    Initial commit

--- src/Draft_Whole_Video1_3_4_0.java
+++ src/Draft_Whole_Video1_3_4_0.java
@@ -1,7 +1,9 @@
 public class Draft_Whole_Video1_3_4_0 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //  закончил видео мин 59 05
 
+        //  видео мин 05 50 - Переменная temp внутри цикла исчезает 20 раз
 
 
     }
