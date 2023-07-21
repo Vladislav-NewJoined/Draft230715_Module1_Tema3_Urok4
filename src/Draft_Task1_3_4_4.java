59f242d3cb3e86bcc077697c62c59a1639e142fd
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-21 14:20:47 +0300

    Initial commit

--- src/Draft_Task1_3_4_4.java
+++ src/Draft_Task1_3_4_4.java
@@ -6,56 +6,18 @@ public class Draft_Task1_3_4_4 {
         System.out.println("Задание: \n4.  Создайте класс, который находит наибольшее из трех чисел. " +
                 "\nКо всем переменным обращаться можно ТОЛЬКО используя this\n\nРешение: ");
         System.out.println("Для решения ниже создан класс 'FindMax'.");
-//        System.out.println("Можно обратиться без слова this. Ниже два примера, с this, и без this, но " +
-//                "без this в нашем случае результат будет null, т.е. некорректным.");
-//        Draft_Task1_3_4_6 s1 = new Draft_Task1_3_4_6();
-//        s1.result(max);
-//        s1.print();
-//    }
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Please enter integer a: ");
-//        int a = scanner.nextInt();
-//        System.out.print("Please enter integer b: ");
-//        int b = scanner.nextInt();
-//        System.out.print("Please enter integer c: ");
-//        int c = scanner.nextInt();
-//
-//        int[] inputList = {a, b, c};
-//        int max = inputList[0];
-//
-//        for (int i = 0; i < inputList.length; i++) {
-//            if (max < inputList[i]) {
-//                max = inputList[i];
-//            }
-//        }
-//        System.out.println("The max number is: " + max);
 
         FindMax findMax = new FindMax();
         findMax.inputIntegers();
 
         findMax.printResult();
-//        void result (int max) {
-//            this.max = max;
-//        }
-//        void print () {
-//            System.out.println("With \"this\", name is: "/* + name*/);
-//        }
     }
 }
 
 class FindMax {
     Scanner scanner = new Scanner(System.in);
-    //        System.out.print("Please enter integer a: ");
-//    int a = scanner.nextInt();
-//        System.out.print("Please enter integer b: ");
-//    int b = scanner.nextInt();
-//        System.out.print("Please enter integer c: ");
-//    int c = scanner.nextInt();
-//    int a = 3;
-//    int b = 8;
-//    int c = 5;
-    int[] inputList;/* = {a, b, c};*/
-    int max;/* = inputList[0];*/
+    int[] inputList;
+    int max;
 
     void inputIntegers() {
         this.scanner = scanner;
